1ddbb55e01e0b9baba10a3fd683b1f5d2847b59e
kohei-san <kohei.miyatake@gmail.com>
2021-03-22 21:26:38 +0900

    ActiveHashのid修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,13 +9,16 @@ class Item < ApplicationRecord
   with_options presence: true do
     validates :name
     validates :info
+    validates :price, numericality: { only_integer: true,
+      greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: 'は¥300- ~ ¥9,999,999で入力してください。'}
+    # 各バリデーション追記必要
+  end
+
+  with_options numericality:{ other_than: 0 } do
     validates :category_id
     validates :sales_status_id
     validates :shipping_fee_id
     validates :prefecture_id
     validates :schedule_delivery_id
-    validates :price
-
-    # 各バリデーション追記必要
   end
 end
