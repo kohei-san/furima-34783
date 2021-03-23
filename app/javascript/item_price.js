a1e32ba7dfc0ba7e6cdc606f037feedd201be22f
kohei-san <kohei.miyatake@gmail.com>
2021-03-23 16:06:13 +0900

    javaScriptファイル追加（価格入力部）

---
+++ app/javascript/item_price.js
@@ -0,0 +1,16 @@
+function taxCalc (){
+  const inputPrice = document.getElementById("item-price");
+  const taxPrice = document.getElementById("add-tax-price");
+  const profit = document.getElementById("profit");
+
+  inputPrice.addEventListener('input', () => {
+    const value = inputPrice.value
+    const tax = Math.floor(value * 0.1);
+    taxPrice.innerHTML = tax;
+    profit.innerHTML = value - tax;
+  })
+} 
+
+window.addEventListener('load', taxCalc )
+
+// letにて都度値を書き換え、出力する?
