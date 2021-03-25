c40d45666cef27dc54d476ef41dc778c8aa3bda0
kohei-san <kohei.miyatake@gmail.com>
2021-03-25 10:57:58 +0900

    itemsコントローラーbefore_actionのリファクタリング

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -46,8 +46,7 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    if user_signed_in? && current_user.id == @item.user.id 
-    else
+    unless current_user.id == @item.user.id 
       redirect_to root_path
     end
   end
