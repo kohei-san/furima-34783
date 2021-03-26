0915da06832c574ac4ac74469df37e5e5717dd5e
kohei-san <kohei.miyatake@gmail.com>
2021-03-26 18:28:12 +0900

    hoge

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -13,7 +13,7 @@ const pay = () => {
       exp_year: `20${formData.get("card_exp_year")}`,
       cvc: formData.get("card_cvc")
     };
-
+    console.log("hoge");
     Payjp.createToken(card, (status, response) => {
       if (status == 200) {
         const token = response.id;
