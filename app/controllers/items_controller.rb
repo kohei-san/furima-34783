a6a9a523d55eef4afa0f508c261ea8d79edd9520
kohei-san <kohei.miyatake@gmail.com>
2021-03-29 00:29:01 +0900

    販売後のボタン非表示、編集機能制限

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,6 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, except:[:index, :show]
   before_action :find_item, only:[:show, :edit, :update, :destroy]
   before_action :move_to_index, only:[:edit, :update, :destroy]
+  before_action :no_edit_for_sold, only:[:edit]
   
   
   def index
@@ -54,6 +55,11 @@ class ItemsController < ApplicationController
     unless current_user.id == @item.user.id 
       redirect_to root_path
     end
+  end
+
+  def no_edit_for_sold
+    @item.order.item_id == @item.id
+    redirect_to root_path
   end
 
 end
