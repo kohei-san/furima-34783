c7d9685bf1675b4c0530dc0d8ef905a1f99c4b91
kohei-san <kohei.miyatake@gmail.com>
2021-03-28 18:25:36 +0900

    orderAddressバリデーション編集

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,7 +1,4 @@
 class Order < ApplicationRecord
-  # attr_accessor :token
-  # attr_accessor :price
-
   belongs_to :user
   belongs_to :item
   has_one    :address
--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -2,18 +2,12 @@ class OrderAddress
   include ActiveModel::Model
   attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :city, :address, :building, :phone_number, :token
   
-  #orderモデルのバリデーション 
-  # validates :user_id,    presence: true
-  # validates :item_id,    presence: true
-
-  #addressモデルのバリデーション
-  with_options presence: true do
-    validates :postal_code  #,   format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
-    validates :prefecture_id  #, numericality { other_than: o, message: 'must be selected' }
+  with_options presence: true do         
+    validates :postal_code,     format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :prefecture_id,   numericality: { other_than: 0, message: 'must be selected' }
     validates :city
     validates :address
-    validates :phone_number  #,  numericality { only_integer: true }, length {maximum: 11}, length { mimimum: 9}
-    # validates :order_id
+    validates :phone_number,    numericality: { only_integer: true }, length: { maximum: 11 }
   end
 
   def save
@@ -21,5 +15,3 @@ class OrderAddress
     Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, address: address, building: building, phone_number: phone_number, order_id: order.id)
   end
 end
-
-#カード情報入力のバリデーションも追記すること
